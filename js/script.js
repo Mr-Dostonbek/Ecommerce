2eddd88189a437d5c524a02067011c1e97d9952d
Mr-Dostonbek <goyipovdoston0@gmail.com>
2021-08-09 18:34:14 +0500

    add man-filter-section..

--- js/script.js
+++ js/script.js
@@ -53,3 +53,52 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+
+
+
+
+
+
+
+filterSelectionMan("allMan")
+function filterSelectionMan(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDivMan");
+  if (c == "allMan") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "showMan");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "showMan");
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+var btnContainer = document.getElementById("myBtnContainerMan");
+var btns = btnContainer.getElementsByClassName("filterBtnMan");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("activeMan");
+    current[0].className = current[0].className.replace(" activeMan", "");
+    this.className += " activeMan";
+  });
+}
